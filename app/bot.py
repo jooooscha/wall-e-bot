2a7cc4bb4f7c92a2d8cd9a1542baa893ce1c5dea
Joscha Loos <j.loos@posteo.net>
2024-10-19 15:28:22 +0200

    load access_token from env

--- app/bot.py
+++ app/bot.py
@@ -1,8 +1,17 @@
 import simplematrixbotlib as botlib
 from app.bot_helper import *
 from time import sleep
+import os
+from dotenv import load_dotenv
+import sys
 
-creds = botlib.Creds("https://matrix.serwm.com", "wall-e", "c))2l(CBrt],~=9Yu?XdALjl7[fw+q(Y/`U.SQ3{SrSgY}?bSQ`2%%`[~2K3S0577s@}eIhF6SA$J%+qADG_T?_]RUS,13K")
+load_dotenv(sys.argv[1])
+
+creds = botlib.Creds(
+    homeserver="https://matrix.serwm.com",
+    username="wall-e",
+    access_token=os.getenv('ACCESS_TOKEN'),
+)
 config = botlib.Config()
 config.join_on_invite = True
 config.encryption_enabled = True
@@ -16,16 +25,15 @@ PREFIX = '!'
 async def reminder(room_id):
     print(f"Wall-e is in room: {room_id}")
 
-    if room_id != "!SbqqbJBFSbDmSzMjIk:serwm.com":
-        return
+    # only run in this one room:
+    if room_id == "!SbqqbJBFSbDmSzMjIk:serwm.com":
+        while True:
+            # sleep until next notification
+            t_wait, t_target = seconds_to_next_notification()
+            print(f"Waiting {t_wait.total_seconds()}, until {t_target.strftime('%d.%m %H:%M')}")
+            sleep(t_wait.total_seconds())
 
-    while True:
-        # sleep until next notification
-        t_wait, t_target = seconds_to_next_notification()
-        print(f"Waiting {t_wait.total_seconds()}, until {t_target.strftime('%d.%m %H:%M')}")
-        sleep(t_wait.total_seconds())
-
-        await send_event_info(room_id)
+            await send_event_info(room_id)
 
 @bot.listener.on_message_event
 async def info(room, message):
