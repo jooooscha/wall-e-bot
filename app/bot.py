745739aebe649cf9d74658d99e60229426df08be
Joscha Loos <j.loos@posteo.net>
2024-10-19 14:39:22 +0200

    reactivate reminder

--- app/bot.py
+++ app/bot.py
@@ -1,5 +1,6 @@
 import simplematrixbotlib as botlib
 from app.bot_helper import *
+from time import sleep
 
 creds = botlib.Creds("https://matrix.serwm.com", "wall-e", "c))2l(CBrt],~=9Yu?XdALjl7[fw+q(Y/`U.SQ3{SrSgY}?bSQ`2%%`[~2K3S0577s@}eIhF6SA$J%+qADG_T?_]RUS,13K")
 config = botlib.Config()
@@ -11,6 +12,20 @@ config.ignore_unverified_devices = True
 bot = botlib.Bot(creds, config)
 PREFIX = '!'
 
+@bot.listener.on_startup
+async def reminder(room_id):
+    print(f"Wall-e is in room: {room_id}")
+
+    if room_id != "!SbqqbJBFSbDmSzMjIk:serwm.com":
+        return
+
+    while True:
+        # sleep until next notification
+        t_wait, t_target = seconds_to_next_notification()
+        print(f"Waiting {t_wait.total_seconds()}, until {t_target.strftime('%d.%m %H:%M')}")
+        sleep(t_wait.total_seconds())
+
+        await send_event_info(room_id)
 
 @bot.listener.on_message_event
 async def info(room, message):
